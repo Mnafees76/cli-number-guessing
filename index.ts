b7e01578f408f43ed79ad998f15b920441e99dc9
Muhammad Nafees <165241321+Mnafees76@users.noreply.github.com>
2024-04-01 16:30:32 +0500

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquire from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 7 + 1);
+ 
+
+const answers = await inquire.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number between 1-7:",
+    },
+]);
+if(answers.userGuessedNumber === randomNumber){
+    console.log("congtratulations! you guessed right number.")
+
+}else {
+    console.log("you guessed wrong number");
+
+}
